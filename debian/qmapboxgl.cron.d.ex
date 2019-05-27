a809bdced2c129571a6cd95eda7a8dbf60eceb20
Rinigus <rinigus.git@gmail.com>
2019-05-27 21:55:28 +0300

    cleanup

--- debian/qmapboxgl.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the qmapboxgl package
-#
-0 4	* * *	root	[ -x /usr/bin/qmapboxgl_maintenance ] && /usr/bin/qmapboxgl_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libqmapboxgl 1.5.0 qmapboxgl (>> 1.5.0-0), qmapboxgl (<< 1.5.0-99)
